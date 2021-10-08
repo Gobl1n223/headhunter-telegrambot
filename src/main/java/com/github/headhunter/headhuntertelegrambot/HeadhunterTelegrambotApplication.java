47db262e160ae879b601937e6674140e74d6dc43
Artem <footerka2@gmail.com>
2021-10-08 15:24:40 +0300

    HHTB skeleton SpringBoot project

---
+++ src/main/java/com/github/headhunter/headhuntertelegrambot/HeadhunterTelegrambotApplication.java
@@ -0,0 +1,13 @@
+package com.github.headhunter.headhuntertelegrambot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeadhunterTelegrambotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeadhunterTelegrambotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/headhunter/headhuntertelegrambot/HeadhunterTelegrambotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.headhunter.headhuntertelegrambot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeadhunterTelegrambotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
